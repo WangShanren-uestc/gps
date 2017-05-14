7c26dbc04fa1473236e6bc53d193253e692b9291
WangShanren-uestc <201522070315@std.uestc.edu.cn>
2017-05-14 11:08:09 +0800

    gps

---
+++ gps/src/gps_agent_pkg/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/w/gps/src/gps_agent_pkg/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu:/home/w/catkin_ws/devel/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/w/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/w/gps/src/gps_agent_pkg/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/w/gps/src/gps_agent_pkg:/home/w/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
